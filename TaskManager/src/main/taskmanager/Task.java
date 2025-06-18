f31c9ec7c4f650aa25b00ca007d6ed39ab0e2247
Rq4n <ryanlimabatista2@gmail.com>
2025-06-18 18:06:24 -0300

    Classe Task

--- TaskManager/src/main/taskmanager/Task.java
+++ TaskManager/src/main/taskmanager/Task.java
@@ -1,4 +1,36 @@
 package main.taskmanager;
 
 public class Task {
+    private int id;
+    private String descricao;
+    private boolean concluida;
+
+    public void Task(int id, String descricao, boolean concluida){
+        this.id = id;
+        this.descricao = descricao;
+        this.concluida = concluida;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+
+    public void setConcluida(boolean concluida){
+        this.descricao = descricao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    public boolean isConcluida() {
+        return isConcluida();
+    }
 }
