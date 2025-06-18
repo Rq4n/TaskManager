09a877cee69be7546b55ea7efd2a4d0fb77a23fd
Rq4n <ryanlimabatista2@gmail.com>
2025-06-18 18:37:57 -0300

    apaguei o teste da classe Task.java que estava na Main.java

--- TaskManager/src/main/taskmanager/Main.java
+++ TaskManager/src/main/taskmanager/Main.java
@@ -2,10 +2,4 @@ package main.taskmanager;
 
 public class Main {
     public static void main(String[] args) {
-        Task task = new Task();
-        task.setId(1);
-        task.setDescricao("Levar o lixo");
-        task.setConcluida(true);
-        System.out.println(task);
-    }
 }
