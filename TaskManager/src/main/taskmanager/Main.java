fab4c84c121a739833eb33e0e19abaf30a1ab656
Rq4n <ryanlimabatista2@gmail.com>
2025-06-18 21:37:55 -0300

    + metodo marcarConcluida

--- TaskManager/src/main/taskmanager/Main.java
+++ TaskManager/src/main/taskmanager/Main.java
@@ -1,13 +1,14 @@
 package main.taskmanager;
-import java.util.ArrayList;
+
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         TaskManager manager = new TaskManager();
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
+            Thread.sleep(1000);
             System.out.println("===== GERENCIADOR DE TAREFAS =====");
             System.out.println(" 1 - ADICIONAR TAREFA");
             System.out.println(" 2 - LISTAR TAREFAS");
@@ -15,7 +16,38 @@ public class Main {
             System.out.println(" 4 - MARCAR TAREFA COMO CONCLUIDA");
             System.out.println(" 0 - SAIR");
             System.out.println(" MARCAR UMA OPCAO:");
+
+            String opcao = scanner.nextLine();
+
+            switch (opcao) {
+                case "1":
+                    System.out.println("Descricao da tarefa: ");
+                    String descricao = scanner.nextLine();
+                    manager.adicionarTarefa(descricao, false);
+                    System.out.println("Tarefa adicionada.");
+                    break;
+
+                case "2":
+                    manager.listarTarefas();
+                    Thread.sleep(2000);
+                    break;
+
+                case "3":
+                    System.out.println("ID da tarefa para remover: ");
+                    int idRemove = Integer.parseInt(scanner.nextLine());
+                    manager.removerTarefa(idRemove);
+                    break;
+
+                case "4":
+                    System.out.println("Qual tarefa deseja concluir? : ");
+                    int idConcluir = Integer.parseInt(scanner.nextLine());
+                    manager.marcarConcluida(idConcluir);
+                    System.out.println("Tarefa concluida");
+                    break;
+
+                case "0":
+                    return;
+            }
         }
-        
     }
 }
--- TaskManager/src/main/taskmanager/TaskManager.java
+++ TaskManager/src/main/taskmanager/TaskManager.java
@@ -1,5 +1,7 @@
 package main.taskmanager;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,27 +10,41 @@ public class TaskManager {
     private int contadorId = 1;
 
     //Adicionar tarefas
-    public void adicionarTarefa(String descricao, boolean concluida){
+    public void adicionarTarefa(String descricao, boolean concluida) {
         Task novaTarefa = new Task(contadorId, descricao, concluida);
-            tarefas.add(novaTarefa);
-            contadorId++;
+        tarefas.add(novaTarefa);
+        contadorId++;
     }
+
     //Listar tarefas
-    public void listarTarefas(){
-        if(tarefas.isEmpty()){
+    public void listarTarefas() {
+        if (tarefas.isEmpty()) {
             System.out.println("Nao tem nenhuma tarefa");
         } else {
             for (Task tarefa : tarefas)
                 System.out.println(tarefa);
         }
     }
+
     //Remover tarefas
-    public void removerTarefa(int id){
-        boolean removido = tarefas.removeIf(tarefas-> tarefas.getId() == id );
-        if (removido){
-            System.out.println("Tarefa:"+id+" Removida");
+    public void removerTarefa(int id) {
+        boolean removido = tarefas.removeIf(tarefas -> tarefas.getId() == id);
+        if (removido) {
+            System.out.println("Tarefa:" + id + " Removida");
         } else {
-            System.out.println("Tarefa: "+id+" Nao encontrado");
+            System.out.println("Tarefa: " + id + " Nao encontrada");
+        }
+    }
+
+    //Atualizar tarefa
+    public void marcarConcluida(int id) {
+        for (Task tarefa : tarefas) {
+            if (tarefa.getId() == id) {
+                tarefa.setConcluida(true);
+                System.out.println("Tarefa: " + id + " concluida");
+            } else {
+                System.out.println("Tarefa: " + id + " nao concluida");
+            }
         }
     }
 }
